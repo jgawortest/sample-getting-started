da46ac04693a67085b128e2d8b8040a4b658b9b2
Jarek Gawor <jgawor@gmail.com>
2023-04-23 01:09:45 -0400

    update

--- src/main/webapp/js/mpData.js
+++ src/main/webapp/js/mpData.js
@@ -22,7 +22,7 @@ function getRuntimeRequest() {
             var version = req.responseText;
             if (version != "") {
                 var appTitle = document.getElementById("appTitle");
-                appTitle.innerText = "Open Liberty " + version + " System Properties Sample 1682220266";
+                appTitle.innerText = "Open Liberty " + version + " System Properties Sample 1682226585";
             }
         }
     };
